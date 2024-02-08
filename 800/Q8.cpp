309d9c917f7ac12062c6e37d27f50b1b1fa065aa
Devansh-Sabharwal <140970037+Devansh-Sabharwal@users.noreply.github.com>
2024-02-09 02:09:18 +0530

    Create Q8.cpp

---
+++ 800/Q8.cpp
@@ -0,0 +1,18 @@
+//https://codeforces.com/contest/1878/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int tc;
+    cin>>tc;
+    while(tc--){
+        int n , k;
+        cin>>n>>k; bool flag = false;
+        for(int i=0;i<n;i++){
+            int x;
+            cin>>x; 
+            if(x==k){flag = true;}
+        }
+        if(flag) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
