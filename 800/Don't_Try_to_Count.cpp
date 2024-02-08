1d34defc69250ef5e35eb0756d4d3cc7c765a8d7
Devansh-Sabharwal <140970037+Devansh-Sabharwal@users.noreply.github.com>
2024-02-08 20:55:00 +0530

    Create Don't_Try_to_Count.cpp

---
+++ 800/Don't_Try_to_Count.cpp
@@ -0,0 +1,20 @@
+//https://codeforces.com/problemset/problem/1881/A
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int tc;
+    cin>>tc;
+    while(tc--){
+        int n,m,k; string x,s;
+        cin>>n>>m;
+        cin>>x>>s;
+        for(k = 0;k<6;k++) {
+            if(x.find(s) != -1){
+                cout<<k<<endl;
+                break;
+            }
+            x+=x;
+        }
+        if(k==6) cout<<-1<<endl;
+    }
+}
