93354cc9dc6f1687eb9568f6a38ef1f432c0adf3
Devansh-Sabharwal <140970037+Devansh-Sabharwal@users.noreply.github.com>
2024-02-09 11:50:10 +0530

    Create Q11.cpp

---
+++ 800/Q11.cpp
@@ -0,0 +1,18 @@
+https://codeforces.com/problemset/problem/1866/A//
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    // int tc;
+    // cin>>tc;
+    // while(tc--){
+        int n;
+        cin>>n;
+        int mini = INT_MAX , maxi=INT_MIN+1;
+        for(int i=0;i<n;i++){
+            int x; cin>>x;
+            if(x<0) maxi = max(maxi,x);
+            else mini = min(mini,x);
+        }
+        cout<<min(abs(maxi),mini)<<endl;
+   // }
+}
