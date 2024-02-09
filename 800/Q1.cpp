c4e1751f92e1db8d1d52fde4d1b9e7d205400470
Devansh-Sabharwal <140970037+Devansh-Sabharwal@users.noreply.github.com>
2024-02-09 11:52:36 +0530

    Create Q1.cpp

---
+++ 800/Q1.cpp
@@ -0,0 +1,17 @@
+https://codeforces.com/contest/1903/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int tc;
+    cin>>tc;
+    while(tc--){
+        int n , k;
+        cin>>n>>k;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        if(k==1 && !is_sorted(arr,arr+n)) cout<<"NO"<<endl;
+        else cout<<"YES"<<endl;
+    }
+}
