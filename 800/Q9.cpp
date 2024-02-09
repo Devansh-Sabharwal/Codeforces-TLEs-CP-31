88f78b50086d8a08949f3672055c02b6e788db30
Devansh-Sabharwal <140970037+Devansh-Sabharwal@users.noreply.github.com>
2024-02-09 10:35:44 +0530

    Create Q9.cpp

---
+++ 800/Q9.cpp
@@ -0,0 +1,16 @@
+//https://codeforces.com/problemset/problem/1877/A
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int tc;
+    cin>>tc;
+    while(tc--){
+        int n,sum=0;
+        cin>>n;
+        for(int i=0;i<n-1;i++){
+            int x; cin>>x;
+            sum+=x;
+        }
+        cout<<(0-sum)<<endl;
+    }
+}
