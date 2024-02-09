a1712b1a7d532b1718825710dd31c05fc9d8ad66
Devansh-Sabharwal <140970037+Devansh-Sabharwal@users.noreply.github.com>
2024-02-09 12:03:21 +0530

    Create 04.cpp

---
+++ 800/04.cpp
@@ -0,0 +1,13 @@
+//https://codeforces.com/problemset/problem/1899/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tc;
+    cin>>tc;
+    while(tc--){
+        int n ; cin>>n;
+        if(n%3==0) cout<<"Second"<<endl;
+        else cout<<"First"<<endl;
+    }
+}
