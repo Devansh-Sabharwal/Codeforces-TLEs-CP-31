ddf26a07acff5e9de85f4d778ef13747b28aa70b
Devansh-Sabharwal <140970037+Devansh-Sabharwal@users.noreply.github.com>
2024-03-07 22:52:32 +0530

    AB Balance

---
+++ 900/20.cpp
@@ -0,0 +1,20 @@
+//https://codeforces.com/problemset/problem/1606/A
+//AB Balance
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define gcd __gcd
+#define ull unsigned long long
+#define endl "\n"
+int solve(){
+}
+int main() {
+	int tc;
+	cin>>tc;
+	while(tc--){
+        string s;
+        cin>>s;
+        s[0] = s.back();
+        cout<<s<<endl;
+	}
+}
