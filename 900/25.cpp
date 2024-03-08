ca99f98a5d803595693b6e945fa86315d0af29a9
Devansh-Sabharwal <140970037+Devansh-Sabharwal@users.noreply.github.com>
2024-03-08 22:29:32 +0530

    Bad Boy

---
+++ 900/25.cpp
@@ -0,0 +1,19 @@
+//https://codeforces.com/contest/1537/problem/B
+//Bad Boy
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define gcd __gcd
+#define ull unsigned long long
+#define endl "\n"
+int solve(){
+}
+int main() {
+	int tc;
+	cin>>tc;
+	while(tc--){
+        int m,n,x,y;
+        cin>>m>>n>>x>>y;
+        cout<<1<<" "<<1<<" "<<m<<" "<<n<<endl;
+	}
+}
