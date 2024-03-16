9e2e939caba9bc2b50af47fdaaa68b9f4ffb10ab
Devansh-Sabharwal <140970037+Devansh-Sabharwal@users.noreply.github.com>
2024-03-16 09:11:19 +0530

    Monsters

--- 1000/05.cpp
+++ 1000/05.cpp
@@ -1,49 +1,32 @@
 //https://codeforces.com/problemset/problem/1849/B
-#include <bits/stdc++.h>
-#ifndef ONLINE_JUDGE
-#include <D:\CP\debug_Template.cpp>
-#else
-#define debug(...)
-#define debugArr(...)
-#endif
-using namespace std;
-#define ll long long 
-#define gcd __gcd
-#define ull unsigned long long
-#define endl "\n"
+//Monsters
 class cmp{
-public:
-    bool operator()(pair<int,int> &p1, pair<int,int> &p2){
-        int a = p1.first; int idx1 = p1.second;
-        int b = p2.first; int idx2 = p2.second;
-        if(a!=b){
-            return a<b;
+public: 
+     bool operator()(pair<int,int> &p1,pair<int,int>&p2){
+        int a = p1.first, b = p1.second;
+        int c = p2.first, d = p2.second;
+        if(a!=c) return a>c;
+        else return b<d;
         }
-        else return idx1>idx2;
-    }
 };
-int solve(){
-}
 int main() {
 	int tc;
 	cin>>tc;
 	while(tc--){
         int n,k;
         cin>>n>>k;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,cmp> pq;
-        vector<int> arr(n),ans;
-        for(int i=0;i<n;i++){ cin>>arr[i]; pq.push({arr[i],i});}
-        debug(pq);
-        debug(pq.top());
-        while(pq.size()){
-            auto x = pq.top(); pq.pop();
-            debug(x.first-k);
-            if(x.first-k<=0) ans.push_back(x.second);
-            else pq.push({x.first-k,x.second});
-            debug(ans);
-            debug(pq);
-        }
-        for(auto x: ans) cout<<x+1<<" ";
-        cout<<endl;
+        vector<int> arr(n);
+        vector<pair<int,int> > p;
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+            if(arr[i]<k);
+            else if(arr[i]%k==0) arr[i] = k;
+            else arr[i]%=k;
+            p.push_back({arr[i],i});
+        } 
+        debug(p);
+        sort(p.begin(),p.end(),cmp());
+        for(auto c: p) cout<<c.second+1<<" ";
+        cout<<endl;        
 	}
 }
